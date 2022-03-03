3aa2e0f4aacda4418ef210e87a6e0769d8dccbf5
Luis Miguel Del Gallego Horta <ldelgallego@playvox.com>
2022-03-03 08:30:49 -0500

    ✨️ S2.32 Adding Routing

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllMeetupsPage = () => {
+  return <div>AllMeetups</div>;
+};
+
+export default AllMeetupsPage;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+const NewMeetupPage = () => {
+  return <div>NewMeetup</div>;
+};
+
+export default NewMeetupPage;
