bcb5dcce1b82aeca505afcc9e11e4d95804647e9
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-03-16 16:33:47 -0500

    :sparkles: S2.36. Adding Even More Components

---
+++ src/components/meetups/MeetupList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MeetupItem from './MeetupItem';
+
+import classes from './MeetupList.module.css';
+
+const MeetupList = (props) => {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem key={meetup.id} {...meetup} />
+      ))}
+    </ul>
+  );
+};
+
+export default MeetupList;
