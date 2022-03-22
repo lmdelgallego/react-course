3ecb8da7f8430ffad26ff91a13da8a82167212a5
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-03-22 16:55:14 -0500

    ✨ S2.44. Using the "useEffect" Hook

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,42 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
 
 const AllMeetupsPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch('https://goalcoach-a4187.firebaseio.com/meetups.json', {
-    method: 'GET',
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-      console.log(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://goalcoach-a4187.firebaseio.com/meetups.json')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          meetups.push({
+            ...data[key],
+            id: key,
+          });
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
