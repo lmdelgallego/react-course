76bac8e797b320554425395f6dab44f6882ef304
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-03-22 16:30:37 -0500

    ✨ S2.40. Preparing The App For Http Requests & Adding a Backend

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,29 +1,59 @@
+import { useRef } from 'react';
 import Card from '../ui/Card';
 
 import classes from './NewMeetupForm.module.css';
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
+  const titleInput = useRef();
+  const imageInput = useRef();
+  const addressInput = useRef();
+  const descriptionInput = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredTitle = titleInput.current.value;
+    const enteredImage = imageInput.current.value;
+    const enteredAddress = addressInput.current.value;
+    const enteredDescription = descriptionInput.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  };
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' />
+          <input type='text' required id='title' ref={titleInput} />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' />
+          <input type='url' required id='image' ref={imageInput} />
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' />
+          <input type='text' required id='address' ref={addressInput} />
         </div>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
-          <textarea name='description' id='description' cols='30' rows='5' required></textarea>
+          <textarea
+            name='description'
+            id='description'
+            cols='30'
+            rows='5'
+            required
+            ref={descriptionInput}
+          ></textarea>
         </div>
         <div className={classes.actions}>
-          <button>Create Meetup</button>
+          <button type='submit'>Create Meetup</button>
         </div>
       </form>
     </Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,12 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const addMeetupHandler = (meetup) => {};
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
