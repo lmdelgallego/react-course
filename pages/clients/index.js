e76c824734dfd576e7cc34163448a42ec7b687f0
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-03-28 11:40:56 -0500

    ✨ S3.62. Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import Link from 'next/link';
 
 const ClientsPage = () => {
+  const clients = [
+    {
+      name: 'Maximilian',
+      id: 'max',
+    },
+    {
+      name: 'Manuel',
+      id: 'manu',
+    },
+    {
+      name: 'Luis Miguel',
+      id: 'luismi',
+    },
+  ];
+
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id} test-id={`link-client-${client.id}`}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
