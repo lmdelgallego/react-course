680080c077990b5e1e18b1cf766933edd97ed8b3
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-03-28 23:17:55 -0500

    ✨ S4.70. Setting Up The Main Pages

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>The Filtered Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
