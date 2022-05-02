2fa519d6ece107aae96c62623c9041b5ba72c86d
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-02 09:12:12 -0500

    ✨ S4.75. Styling Components In Next.js Projects

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,6 @@
-import Link from "next/link"
+import Link from "next/link";
+
+import classes from './event-item.module.css';
 
 const EventItem = ({event,...props}) => {
   const {title, image, date, location, id} = event;
@@ -14,19 +16,19 @@ const EventItem = ({event,...props}) => {
 
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image } alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
