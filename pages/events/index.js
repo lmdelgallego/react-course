1ad29cfafefdc332ec6ef92ab72dae0013c725a2
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-04 07:52:26 -0500

    ✨ S4.80. Adding a Filter Form for Filtering Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/event-search';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
   return (
-    <div>
+    <Fragment>
+      <EventSearch/>
       <EventList items={events}/>
-    </div>
+    </Fragment>
   );
 };
 
