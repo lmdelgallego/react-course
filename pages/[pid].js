9a8c4df1a8bbc2895f6cf48d1b94ed85e486de1e
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-08 18:44:20 -0500

    ✨ S5.101. Working With Fallback Pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,10 @@ import { Fragment } from "react"
 
 const ProductDetailPage = (props) => {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>
+  }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -34,10 +38,8 @@ export const getStaticPaths = async () => {
   return {
     paths: [
       { params: { pid: 'p1' } },
-      { params: { pid: 'p2' } },
-      { params: { pid: 'p3' } },
     ],
-    fallback: false,
+    fallback: true, // false, true, or 'blocking'
   }
 }
 
