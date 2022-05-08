dcdcea9e5dc3d19c086307d6d8288a726b81e5e0
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-08 17:50:37 -0500

    ✨ S5.96. A Closer Look At "getStaticProps" & Configuration Options

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,29 @@ const HomePage = (props) => {
   );
 };
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log('(Re)building products...');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (data.producs.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products
     },
-    revalidate: 10
+    revalidate: 10,
   };
 }
 
