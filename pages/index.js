90f60a3042217ee00bc0218f379730f8a0dd5ef0
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-08 17:18:16 -0500

    ✨ S5.89. Init PreRender

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
-import Link from 'next/link';
-
 const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
       <ul>
-        <li>
-          <Link href='/portfolio'>Portfolio</Link>
-        </li>
-        <li>
-          <Link href='/clients'>Clients</Link>
-        </li>
+        <li> Project 1 </li>
+        <li> Project 2 </li>
+        <li> Project 3 </li>
       </ul>
     </div>
   );
