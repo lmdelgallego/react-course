f45bbb3643169a2a904da8cf9962c0cf91486480
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-15 12:00:10 -0500

    ✨ S5.112. Using the "useSWR" NextJS Hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,34 +1,50 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 const LastSalesPage = () => {
 
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR('https://goalcoach-a4187.firebaseio.com/sales.json', (url) => fetch(url).then(res => res.json()));
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch('https://goalcoach-a4187.firebaseio.com/sales.json')
-      .then(res => res.json())
-      .then(data => {
-        const transformedData = [];
-        for (let key in data) {
-          transformedData.push({
-            id: key,
-            ...data[key],
-          });
-        }
-        console.log('transformedData', transformedData);
-        setSales(transformedData);
-        setIsLoading(false);
-      });
-  }, []);
+    if (data) {
+      const transformedData = [];
+      for (let key in data) {
+        transformedData.push({
+          id: key,
+          ...data[key],
+        });
+      }
+      console.log('transformedData', transformedData);
+      setSales(transformedData);
+    }
+  }, [data]);
 
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch('https://goalcoach-a4187.firebaseio.com/sales.json')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       const transformedData = [];
+  //       for (let key in data) {
+  //         transformedData.push({
+  //           id: key,
+  //           ...data[key],
+  //         });
+  //       }
+  //       console.log('transformedData', transformedData);
+  //       setSales(transformedData);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
-  if (!sales) {
+  if (error) {
     return <h1>No sales found</h1>;
+  }
+  if (!data || !sales) {
+    return <h1>Loading...</h1>;
   }
 
   return (
