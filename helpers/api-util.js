5f1eed418efbebfa453400517599de18c7071830
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-15 16:01:08 -0500

    ✨ S6.119. Loading Data & Paths For Dynamic Pages

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -2,19 +2,20 @@ export const getAllEvents = async () => {
   const response = await fetch('https://goalcoach-a4187.firebaseio.com/events.json');
   const data = await response.json();
 
-  const events = Object.keys(data).map((key) => {
+  return Object.keys(data).map((key) => {
     return {
       ...data[key],
       id: key,
     };
   });
-
-  console.log('events', events);
-
-  return events;
 }
 
 export const getFeaturedEvents = async () => {
   const allEvent = await getAllEvents();
   return allEvent.filter((event) => event.isFeatured);
+}
+
+export const getEventById = async (id) => {
+  const allEvent = await getAllEvents();
+  return allEvent.find((event) => event.id === id);
 }
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,11 @@
-import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import ErrorAlert from '../../components/ui/error-alert';
 
-const EventDetailPage = () => {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+const EventDetailPage = ({ event, ...props }) => {
 
   if (!event) {
     return <ErrorAlert><p>Event not found</p></ErrorAlert>;
@@ -26,5 +21,33 @@ const EventDetailPage = () => {
     </Fragment>
   );
 };
+
+export const getStaticProps = async (context) => {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+  return {
+    props: {
+      event
+    }
+  };
+}
+
+export const getStaticPaths = async () => {
+
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => {
+    return {
+      params: {
+        eventId: event.id
+      }
+    }
+  });
+
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 export default EventDetailPage;
