fc9081d33d072d29bd9d2b9d6a116a51974899ae
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-05-20 18:18:55 -0500

    ✨ S7.131. Working with the "_app.js" File (and Why)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import Layout from '../components/layout/layout'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return <Layout>
+    <Head>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    <Component {...pageProps} />
+  </Layout>
 }
 
 export default MyApp
